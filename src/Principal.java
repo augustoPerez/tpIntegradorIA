e4bbac8cf9130a67775af7940eecc47cbb4b7283
Augusto <augusto.perez.995@gmail.com>
2017-10-17 14:16:09 -0300

    Actualizacion

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,15 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Principal {
 
     public static void main(String[] args) {
 
-        int[] tableroInicial = {1, 2, 3, 4, 0, 5, 6, 7, 8}; //tablero inicial
+//        int[] tableroInicial = {1, 2, 3, 4, 0, 5, 6, 7, 8}; //tablero inicial
 
 //        Arbol arbol = CrearArbol.maxNivel(tableroInicial, 30, 3);
-//        Arbol arbol = CrearArbol.enAmplitud(tableroInicial);
-//        Arbol arbol = CrearArbol.enProfundidad(tableroInicial);
 
-//        int[] asd = {1, 5, 7, 6, 0, 4, 8, 3, 2};  Una disposición de tablero no encontrada
-        int[] asd = {1, 5, 7, 6, 0, 4, 8, 2, 3};
-//        int[] asd = {1, 3, 5, 4, 7, 2, 6, 0, 8};
+//        int[] asd = {1, 5, 7, 6, 0, 4, 8, 3, 2};  Disposición de tablero NO encontrada
+//        int[] asd = {1, 5, 7, 6, 0, 4, 8, 2, 3}; Disposición de tablero encontrada
 
         Scanner scanner = new Scanner(System.in);
 
